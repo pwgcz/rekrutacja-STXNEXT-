575466578be27a240a354ff08704c549292bd361
Pawel Grabczak <grabczak.pa@gmail.com>
2020-07-31 11:44:26 +0200

    update admin & make migrations

--- bookAPI/admin.py
+++ bookAPI/admin.py
@@ -2,12 +2,6 @@ from django.contrib import admin
 from .models import Book, Category, Author
 
 
-class BookAdmin(admin.ModelAdmin):
-    empty_value_display = 'unknown'
-    fields = (
-        'book_id', 'title', 'published_date', 'average_rating', 'ratings_count', 'thumbnail', 'authors', 'categories')
-
-
 class CategoryAdmin(admin.ModelAdmin):
     empty_value_display = 'unknown'
     fields = ('name',)
@@ -18,6 +12,11 @@ class AuthorAdmin(admin.ModelAdmin):
     fields = ('name',)
 
 
-admin.site.register(Book, BookAdmin)
+class BookAdmin(admin.ModelAdmin):
+    empty_value_display = 'unknown'
+    fields = ('title', 'published_date', 'average_rating', 'ratings_count', 'thumbnail', 'authors', 'categories')
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
