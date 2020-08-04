240d417afbfa9476eb6b5fae1d582f1b4aea9c8e
Pawel Grabczak <grabczak.pa@gmail.com>
2020-08-04 17:22:30 +0200

    update serializers

--- bookAPI/serializers.py
+++ bookAPI/serializers.py
@@ -2,19 +2,6 @@ from .models import Book, Category, Author
 from rest_framework import serializers
 
 
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ['book_id',
-                  'title',
-                  'published_date',
-                  'average_rating',
-                  'ratings_count',
-                  'thumbnail',
-                  'authors',
-                  'categories']
-
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
@@ -25,3 +12,24 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = ['author_id', 'name']
+
+
+class BookSerializer(serializers.ModelSerializer):
+    authors = serializers.SlugRelatedField(many=True,
+                                           read_only=True,
+                                           slug_field='name')
+
+    categories = serializers.SlugRelatedField(many=True,
+                                              read_only=True,
+                                              slug_field='name')
+
+    class Meta:
+        model = Book
+        fields = ['book_id',
+                  'title',
+                  'authors',
+                  'published_date',
+                  'categories',
+                  'average_rating',
+                  'ratings_count',
+                  'thumbnail']
