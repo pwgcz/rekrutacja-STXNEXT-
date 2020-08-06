e24f3ab5a9fe771f0b07618dba3b9e1c1ec5a004
Pawel Grabczak <grabczak.pa@gmail.com>
2020-08-06 20:24:31 +0200

    host

--- books/settings.py
+++ books/settings.py
@@ -11,7 +11,7 @@ DEBUG = config("DEBUG", default=True, cast=bool)
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    "https://recruitment-stx-library.herokuapp.com",
+    "https://recruitment-stx-library.herokuapp.com/",
 ]
 
 APPEND_SLASH = True
