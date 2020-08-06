9fd16f0395bd8d6f977252509c8c9454c62a3d78
Pawel Grabczak <grabczak.pa@gmail.com>
2020-08-06 23:15:17 +0200

    update models

--- bookAPI/models.py
+++ bookAPI/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 class Book(models.Model):
     book_id = models.CharField(_("id"), primary_key=True, max_length=50)
 
-    title = models.CharField(_("title"), max_length=200)
+    title = models.CharField(_("title"), max_length=300)
     published_date = models.CharField(
         _("published date"), max_length=300, null=True, blank=True
     )
