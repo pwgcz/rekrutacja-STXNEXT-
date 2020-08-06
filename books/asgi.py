365c73109d6f76f6c6dd027da3c2ba9515d49e72
Pawel Grabczak <grabczak.pa@gmail.com>
2020-08-06 18:58:17 +0200

    add Cling oject

--- books/asgi.py
+++ books/asgi.py
@@ -1,7 +1,8 @@
 import os
 
 from django.core.asgi import get_asgi_application
+from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "books.settings")
 
-application = get_asgi_application()
+application = Cling(get_asgi_application())
